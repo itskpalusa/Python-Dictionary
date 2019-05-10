111a27d8941514b1c0188fd6e60907da941c62d1
Karthik Palusa <karthik.palusa@gmail.com>
2019-05-09 18:04:11 -0600

    Adds func to check if word is in db w/ no error

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
+#import json
 import json
+
+# import data json file for dictionary word
 data = json.load(open("data.json"))
 
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist within the database."
 
 
 word = input("Enter word: ")
