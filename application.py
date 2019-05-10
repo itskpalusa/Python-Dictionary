79bb350e835cf2bba30cde3a85cadc72057db5fb
Karthik Palusa <karthik.palusa@gmail.com>
2019-05-09 18:27:56 -0600

    basic word check algorithm

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 #import json
 import json
 
+# import difflib to compare words
+from difflib import get_close_matches
+
 # import data json file for dictionary word
 data = json.load(open("data.json"))
 
@@ -12,12 +15,22 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        wordCheck = input("Did you mean %s instead? Enter Y if yes, or N if no." %
+                          get_close_matches(word, data.keys())[0])
+        if wordCheck == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif wordCheck == "N":
+            return "The word doesn't exist within the database."
+        else:
+            return "Please doublecheck your entry."
+
     else:
         return "The word doesn't exist within the database."
 
 
-# Gather input from user
+# Get word from user
 word = input("Enter word: ")
 
-# Print defineition
+# Print definition
 print(translate(word))
